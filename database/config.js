420a6157443328e6011f5eec97abc030b3952979
Manjunath2017 <manjunath.jadhav1231@gmail.com>
2020-01-23 23:00:05 +0530

    config

--- database/config.js
+++ database/config.js
@@ -8,7 +8,9 @@ mongoose.set('useCreateIndex', true);
 
 var url = 'mongodb://localhost:27017/nodeblog';
 // var url = 'mongodb://localhost:27017/bysMean';
-mongoose.connect(process.env.DB_CONNECTION||url,{ useNewUrlParser: true }, (error)=>{
+// mongoose.connect(process.env.DB_CONNECTION || url,{ useNewUrlParser: true }, (error)=>{
+
+mongoose.connect(url,{ useNewUrlParser: true }, (error)=>{
     if(!error){
         console.log(' Db connected!!! \n');
     }
